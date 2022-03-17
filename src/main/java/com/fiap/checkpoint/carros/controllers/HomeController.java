417da71e7fb138c9600380d4cece0fae8349377f
Thiago <thiago.silva972@etec.sp.gov.br>
2022-03-16 22:16:08 -0300

    Adição de mais carros, alteração de favicon e título na página, refatoração e ícone de Aro

--- src/main/java/com/fiap/checkpoint/carros/controllers/HomeController.java
+++ src/main/java/com/fiap/checkpoint/carros/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package com.fiap.checkpoint.carros.controllers;
 
 import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,20 +12,20 @@ import com.fiap.checkpoint.carros.model.Carro;
 
 @Controller
 public class HomeController {
-	
 	@GetMapping("/")
 	public ModelAndView home(HttpServletRequest req) {
 		ModelAndView modelAndView = new ModelAndView("home");
 		
-		Carro carro1 = new Carro("Fiat", "Uno", 2022, 4, 15, "Flex");
-		Carro carro2 = new Carro("Chevrolet", "Onix", 2018, 4, 15, "Flex");
-		Carro carro3 = new Carro("Volkswagen", "Gol", 2002, 2, 13, "Gasolina");
-		Carro carro4 = new Carro("Chevrolet", "Monza", 2000, 2, 13, "Álcool");
-		Carro carro5 = new Carro("Peugeot", "306", 2010, 4, 13, "Gasolina");
-		
-		List<Carro> carros = Arrays.asList(carro1, carro2, carro3, carro4, carro5);
-		
-		modelAndView.addObject("carros", carros);
+		modelAndView.addObject("carros", Arrays.asList(
+				new Carro("Fiat", "Uno", 2022, 4, 15, "Flex"), 
+				new Carro("Chevrolet", "Onix", 2018, 4, 15, "Flex"), 
+				new Carro("Volkswagen", "Gol", 1998, 2, 13, "Gasolina"), 
+				new Carro("Chevrolet", "Monza", 2000, 2, 13, "Álcool"), 
+				new Carro("Peugeot", "306", 2010, 4, 13, "Gasolina"), 
+				new Carro("Hyundai", "HB20", 2021, 4, 14, "Flex"), 
+				new Carro("Renault", "Kwid", 2014, 4, 14, "Flex"), 
+				new Carro("Volkswagen", "Voyage", 1995, 4, 14, "Álcool")
+		));
 		
 		return  modelAndView;
 	}
